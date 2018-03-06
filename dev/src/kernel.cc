a49515f678ea62f38bfac6246f97ec501b52ab22
SketchesJavax <32058792+SketchesJavax@users.noreply.github.com>
2018-03-06 21:08:33 +0000

    Added kernel.cc, moved boot.s

---
+++ dev/src/kernel.cc
@@ -0,0 +1,96 @@
+#include <stddef.h> //we can use it: it doesnt use any platform-related api functions
+#include <stdint.h> //include it to get int16_t and some integer types
+#include <stdbool.h>
+
+/* Hardware text mode color constants. */
+enum vga_color {
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
+};
+
+uint8_t make_color(enum vga_color fg, enum vga_color bg) {
+	return fg | bg << 4;
+}
+
+uint16_t make_vgaentry(char c, uint8_t color) {
+	uint16_t c16 = c;
+	uint16_t color16 = color;
+	return c16 | color16 << 8;
+}
+
+size_t strlen(const char* str) {
+	size_t ret = 0;
+	while ( str[ret] != 0 )
+	ret++;
+	return ret;
+}
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 24;
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+
+void tInitialize() {
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
+	{
+		for ( size_t x = 0; x < VGA_WIDTH; x++ )
+		{
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+		}
+	}
+}
+
+void tSetCol(uint8_t color) {
+	terminal_color = color;
+}
+
+void tPutEntryAt(char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = make_vgaentry(c, color);
+}
+
+void tPutChar(char c) {
+	tPutEntryAt(c, terminal_color, terminal_column, terminal_row);
+	if ( ++terminal_column == VGA_WIDTH )
+	{
+		terminal_column = 0;
+		if ( ++terminal_row == VGA_HEIGHT )
+		{
+			terminal_row = 0;
+		}
+	}
+}
+
+void tWriteString(const char* data) {
+	size_t datalen = strlen(data);
+	for ( size_t i = 0; i < datalen; i++ )
+	tPutChar(data[i]);
+}
+
+void kernel_main() {
+	tInitialize();
+	tWriteString("Well... this is OtterOS so far!");
+	for(;;);
+}
