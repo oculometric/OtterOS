3f1b10031394d12fb985479dacf818cac497f64c
Jake Costen <32058792+SketchesJavax@users.noreply.github.com>
2018-03-21 13:03:59 +0000

    Add setGlobal

--- dev/src/include/globals.cc
+++ dev/src/include/globals.cc
@@ -22,10 +22,21 @@
 char* names[255] = {"coshv", "bootmode"};
 char* values[255] = {"0.1", "graphics"};
 
-char* getValueForKey (char* key) {
+char* getGlobal (char* key) {
   int contain = contains (names, key);
   if (contain != -1) {
     return values[contain];
   }
   return "";
+}
+
+void setGlobal (char* key, char* value) {
+  int contain = contains (names, key);
+  if (contain != -1) {
+    values[contain] = value;
+  } else {
+    int len = arraylen (names);
+    names [len] = key;
+    values [len] = key;
+  }
 }
