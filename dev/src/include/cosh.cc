e40495c30f0a342fbdf97252d8baa03bce6214e7
SketchesJavax <32058792+SketchesJavax@users.noreply.github.com>
2018-03-23 10:46:23 +0000

    Worked on stuff

--- dev/src/include/cosh.cc
+++ dev/src/include/cosh.cc
@@ -14,12 +14,30 @@
  * terms of the LICENSE, found in the top level directory.
  */
 
+string argumentString;
+
+void splitCommandAndArgs (char** returner) {
+	int n;
+	println (currentInLine);
+	for (n = 0; n < 4; n++) {
+		char c;
+		returner[0][n] = c = currentInLine[n];
+		tPutChar (currentInLine[n]);
+		tPutChar (c);
+		tPutChar (returner[0][n]);
+	}
+	println (returner[0]);
+	for (n = 4; currentInLine[n] != NULL; n++) {
+		returner[1][n-4] = currentInLine[n];
+	}
+}
+
 // ========== BEGIN COMMAND FUNCTIONS ========== //
 
 static void clib () {
 	int it;
-	for (it = 0; splitLine[1][it] != NULL; it++) {
-		clipboard[it] = splitLine[1][it];
+	for (it = 0; argumentString[it] != NULL; it++) {
+		clipboard[it] = argumentString[it];
 	}
 	for (it=it;clipboard[it] != NULL; it++) {
 		clipboard[it] = NULL;
@@ -27,7 +45,7 @@ static void clib () {
 }
 
 static void modv () {
-	setGlobal (splitLine[1], splitLine[2]);
+	//setGlobal (splitLine[1], splitLine[2]);
 }
 
 static void bell () {
@@ -35,7 +53,7 @@ static void bell () {
 }
 
 static void echo () {
-	println (getNthItemOf(currentInLine, ' ', 1));
+	println (argumentString);
 }
 
 static void cosh () {
@@ -56,21 +74,33 @@ void (* functions [])() = { echo,   bell,   cosh,   clib,   modv};
 
 void executeLine () {
 	char* cmd = currentInLine;
+	//if (strlen(currentInLine) > 4) {
+		string* tmp;
+		char a[512];
+		char b[512];
+		char *foo[2] = { a, b};
+		allocarr (foo, 512, 2);
 
+		splitCommandAndArgs (tmp);
+		cmd = tmp[0];
+		argumentString = tmp[1];
+	//}
 
-	for (int n = 0; n < 10; n++) {
-		for (int i = 0; i < 512; i++) {
-			splitLine[n][i] = '\0';
-		}
-	}
+	// for (int n = 0; n < 10; n++) {
+	// 	for (int i = 0; i < 512; i++) {
+	// 		splitLine[n][i] = '\0';
+	// 	}
+	// }
 	//splitLine[0] = "cosh";
 	//allocarr(splitLine, 512, 10);
-	splitStr(currentInLine, ' ', splitLine);
+	//splitStr(currentInLine, ' ', splitLine);
 	// for (int n = 0; n < 10; n++) {
 	// 	println (splitLine[n]);
 	// }
 
 	//cmd = splitLine[0];
+
+
 
 	int contain = contains (functionNames, cmd);
 	if (contain != -1) {
