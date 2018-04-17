df2593b0a0b718642e87dbd6d16e5c1123522afb
Jake Costen <32058792+SketchesJavax@users.noreply.github.com>
2018-04-17 11:13:13 +0100

    Create hexium.cc

---
+++ deve/src/hexium/hexium.cc
@@ -0,0 +1,16 @@
+/* # Kernel.cc #
+ *
+ * ## Description ##
+ * The core code for OtterOS. Every other part of the project
+ * branches from this starting point.
+ *
+ * ## Info ##
+ * Created 14/03/18
+ * Format C++ source
+ * Written by JavaxCosten
+ *
+ * ## Copyright ##
+ * Copyright (C) 2018 OtterOS Project - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the LICENSE, found in the top level directory.
+ */
