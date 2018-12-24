e9867adff386696acf9325f0235203b9507e1c2c
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-12-24 09:02:53 +0000

    Declarations

--- deve/src/cpp/declarations.h
+++ deve/src/cpp/declarations.h
@@ -20,12 +20,15 @@
 
 bool shouldContinue = true;
 
+// utils.cc
 void intToString (char *buf, unsigned long int n, int base);
 
+// memory.cc
 void memset(char *dest, char src);
 void memset(char *dest, char src, int len);
 void memcpy(char *dest, char *src, int length);
 
+// string.cc
 int strlen (char *s);
 bool strcmp (const char *dst, char *src);
 void strcpy (char *dst, const char *src);
@@ -33,16 +36,25 @@ void strcat (char *dest, const char *src);
 void strncpy (char *destString, const char *sourceString, int maxLength);
 bool strncmp (const char *s1, const char *s2, int c);
 
-void logChar (char c);
-void logInt (int i);
-void logHex (int i);
-void log (char *s);
+// log.cc
+void logChar (char c); // Need fixing
+void logInt (int i); //
+void logHex (int i); //
+void log (char *s); //
 
+// utils.cc
+void switchToRealMode (); // Need implementing
+void switchToProtectedMode (); //
+void executeFunctionInProtectedMode (void *func); //
+
+// time.cc
 void sleep (int delay);
 
+// log.cc
 void fatal (char *msg);
 void warn (char *msg);
 
+// otterKernel.cc
 class OtterKernel {
 public:
 	void prepare ();
